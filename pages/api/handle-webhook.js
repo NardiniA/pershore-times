e0f68fbad978270736c4b8e5c970636072c0692c
NardiniA <techteacher.pershore@gmail.com>
2022-08-29 12:27:44 +0100

    Updated Webhook test 2

--- pages/api/handle-webhook.js
+++ pages/api/handle-webhook.js
@@ -1,5 +1,11 @@
 export default (req, res) => {
-    console.log(req);
+    if (!req.body.event) res.status(400).send({ message: "Invalid Body Params" });
+    if (!req.body.success) res.status(500).send({ message: req.body.error || "Unknown Error" });
 
-    res.status(200).send({ message: "Received!" });
+    if (req.body.event === "post_publish") {
+        console.log("Post Published");
+        res.status(200).send({ message: "Published" });
+    }
+
+    res.status(500).send({ message: "Event not handled" });
 }
