ca060889c4d288d691a0e00c1cdea0a391d63d81
NardiniA <techteacher.pershore@gmail.com>
2022-10-27 15:20:26 +0100

    Fixed bug

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,5 +1,5 @@
 export default function handler(req, res) {
-    if (res.method === "POST") {
+    if (req.method === "POST") {
         res.status(200).json({ name: "John Doe" });
     } else {
         res.status(405).json({ message: "Invalid Method" });
